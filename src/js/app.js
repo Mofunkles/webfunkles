910ead23ecc5af262380b1a52b0de6b34494ac3b
Mofunkles <danseddon@hotmail.co.uk>
2021-04-03 22:47:36 +0100

    implemented gallery demonstration page

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,1 @@
-const randomElement = document.querySelector('.random');
 
-const randomNumber = function () {
-  this.textContent = Math.floor(Math.random() * 100);
-};
-
-randomElement?.addEventListener('click', randomNumber);
